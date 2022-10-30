2a89a4db0241b0051cfc13329a2ee6ac9afa50c9
Paulo Guilherme <pauloguilherme@usp.br>
2022-10-30 10:49:50 -0300

    Add .gitignore

--- Hibredu/Hibredu/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Hibredu/Hibredu/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -118,7 +118,7 @@ namespace Hibredu {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Hibredu;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Hibredu;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
